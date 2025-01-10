741d30ccae2154d0f58b163f0a22129739f48858
nrmliGit <mammadliglnr15@gmail.com>
2025-01-10 02:48:00 -0800

    second

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,38 @@
 import { ArrowRight, ChevronRight, Menu } from "lucide-react";
 import { Link } from "react-router";
 import styles from "../styles/style.module.css";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [isOpened, setIsOpened] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsOpened(!isOpened);
+  };
+
+  const checkScreenSize = () => {
+    if (window.matchMedia("(max-width: 980px)").matches) {
+      setIsOpened(false);
+    }
+  };
+
+  useEffect(() => {
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+    return () => {
+      window.removeEventListener("resize", checkScreenSize);
+    };
+  }, []);
+
   return (
     <div className={styles.header}>
       <div className="flex mx-auto w-[90%] justify-between py-[35px] ">
         <Link to="/" className="text-[#0061f2] text-[18px] font-bold">
           SB UI Kit Pro
         </Link>
-        <div className={styles.menu}>
+        <button onClick={handleMenuClick} className={styles.menu}>
           <Menu size={20} color="#0000008c" />
-        </div>
+        </button>
         <div className={styles.nav}>
           <nav className="flex gap-[52px]">
             <ul className="flex text-[#0000008c] text-[16px] [&>li]:p-[8px] [&>li]:font-medium gap-1 [&>li]:flex">
@@ -34,7 +55,11 @@ export default function Header() {
           </nav>
         </div>
       </div>
-      <div className={styles.littleNav}>
+      <div
+        className={`${styles.littleNav} ${
+          isOpened ? `${styles.openMenu}` : ""
+        }`}
+      >
         <nav className="px-[20px]">
           <ul className=" text-[#0000008c] text-[16px] [&>li]:p-[8px] [&>li]:font-medium gap-1 [&>li]:flex">
             <li className="hover:text-black">Home</li>
